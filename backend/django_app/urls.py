3302022614dfb243bcee66cd9ef04fb16aec2fe6
Taufik Ansari <taufikansari12b@gmail.com>
2025-03-01 04:07:35 +0530

    Modify /hello/ endpoint to return personalized JSON response instead of simple HTTP response

--- backend/django_app/urls.py
+++ backend/django_app/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
+from django.http import JsonResponse
+
 
 def hello_world(request):
-    return HttpResponse("Hello, world! This is our interneers-lab Django server.")
+    name = request.GET.get("name", "world")  # Get 'name' from query params, default to 'world'
+    # return HttpResponse(f"Hello, {name}! This is our interneers-lab Django server.")
+    return JsonResponse({"message": f"Hello, {name}!"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
